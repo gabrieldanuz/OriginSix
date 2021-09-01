815b1572f9e9bcee9fef9bfd67e5784e6b92cb3c
Gabriel Oliveira <gabrieldanuzoliveira358@gmail.com>
2021-08-31 21:30:51 -0300

    Bora pro terceiro video

--- main.js
+++ main.js
@@ -6,3 +6,22 @@ for (const element of toggle) {
         nav.classList.toggle('show')
     })
 }
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links ) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
+    })
+}
+
+ const header = document.querySelector("#header")
+ const navHeight = header.offsetHeight
+
+ window.addEventListener('scroll',function() {
+     if (window.scrollY >= navHeight) {
+        header.classList.add ('scroll')
+     }else {
+        header.classList.remove ('scroll')
+     }
+ } )
